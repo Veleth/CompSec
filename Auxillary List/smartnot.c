ca59041a7a9bf3be02683305fe98cc2106c9d4c6
Gabriel Tanski <236346@student.pwr.edu.pl>
2019-01-25 19:51:04 +0100

    Task1

---
+++ Auxillary List/smartnot.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+//gcc smartnot.c -o prog -m32 -fno-stack-protector -z execstack
+
+/*
+char shellcode[]= "\x31\xc0\x50\x68\x2f\x2f\x73"
+                   "\x68\x68\x2f\x62\x69\x6e\x89"
+                   "\xe3\x89\xc1\x89\xc2\xb0\x0b"
+                   "\xcd\x80\x31\xc0\x40\xcd\x80";
+*/
+
+char shellcode[]= "\xe9\x1e\x00\x00\x00"
+    "\xb8\x04\x00\x00\x00"
+    "\xbb\x01\x00\x00\x00"
+    "\x59"
+    "\xba\x0f\x00\x00\x00"
+    "\xcd\x80"
+    "\xb8\x01\x00\x00\x00"
+    "\xbb\x00\x00\x00\x00"
+    "\xcd\x80"
+    "\xe8\xdd\xff\xff\xff"
+    "236346\r\n";
+
+int main(int argc, char* argv[]){
+	int *ret;
+	ret = (int *)&ret +2;
+	(*ret) = (int) shellcode;
+}
